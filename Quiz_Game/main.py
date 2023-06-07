5969556b67670bdf04fe7e1716526e4f66313a13
paras-017 <paraschauhan0017@gmail.com>
2023-06-07 20:24:27 +0530

    quiz game added

--- Quiz_Game/main.py
+++ Quiz_Game/main.py
@@ -1,39 +1,14 @@
-from pytube import Playlist
-import os
-import re
+import que
+# print(que.data["questions"][0])
+count=0
+for q in que.data["questions"]:
+    print(q['question'])
+    print(f"a:{q['options']['a']}")
+    print(f"b:{q['options']['b']}")
+    print(f"c:{q['options']['c']}")
+    print(f"d:{q['options']['d']}\n")
+    candidate = input('Answer: ')
+    print('\n')
+    if(candidate == q['correctOption']): count+=1
 
-def download(playlist):
-    progress = None
-    PATH = f'./{title}'
-    with open(PATH+'/dependList.txt','r') as f:
-        progress = int(f.read())
-
-    videos = playlist.videos
-    for i,video in enumerate(videos):
-        if i >= progress:
-            v = video.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first()
-            v.download(PATH+'/')
-            with open(PATH+'/dependList.txt','w') as f:
-                f.write(f'{i+1}')
-        print(f'Downloaded {i+1} video')
-
-link = input('Enter link:')
-pl = Playlist(link)
-total_videos = len(pl.videos)
-print(f'Total Videos:{total_videos}')
-title = ' '.join(re.findall("[a-zA-Z]+", pl.title))
-print(title)
-
-if os.path.exists(title):
-    length_file = len(os.listdir(title)) - 1
-    if length_file!=total_videos:
-        download(pl)
-        print("DONE")
-    else:
-        print('Already Exist')
-else:
-    os.makedirs(f'./{title}')
-    with open(f'./{title}/dependList.txt','w') as f:
-        f.write('0')
-    download(pl)
-    print('DONE')
+print(f'You got {count} marks out of {len(que.data["questions"])}')
---
+++ Quiz_Game/que.py
@@ -0,0 +1,94 @@
+data ={
+  "questions": [
+    {
+      "question": "What is the capital of France?",
+      "options": {
+        "a": "London",
+        "b": "Paris",
+        "c": "Berlin",
+        "d": "Rome"
+      },
+      "correctOption": "b"
+    },
+    {
+      "question": "Which planet is known as the Red Planet?",
+      "options": {
+        "a": "Mars",
+        "b": "Venus",
+        "c": "Jupiter",
+        "d": "Saturn"
+      },
+      "correctOption": "a"
+    },
+    {
+      "question": "Who painted the Mona Lisa?",
+      "options": {
+        "a": "Leonardo da Vinci",
+        "b": "Pablo Picasso",
+        "c": "Vincent van Gogh",
+        "d": "Claude Monet"
+      },
+      "correctOption": "a"
+    },
+    {
+      "question": "Who wrote the novel 'Pride and Prejudice'?",
+      "options": {
+        "a": "Jane Austen",
+        "b": "Charlotte Bronte",
+        "c": "Emily Dickinson",
+        "d": "Virginia Woolf"
+      },
+      "correctOption": "a"
+    },
+    {
+      "question": "What is the chemical symbol for gold?",
+      "options": {
+        "a": "Au",
+        "b": "Ag",
+        "c": "Fe",
+        "d": "Hg"
+      },
+      "correctOption": "a"
+    },
+    {
+      "question": "Who is known as the 'Father of Physics'?",
+      "options": {
+        "a": "Isaac Newton",
+        "b": "Albert Einstein",
+        "c": "Galileo Galilei",
+        "d": "Nikola Tesla"
+      },
+      "correctOption": "a"
+    },
+    {
+      "question": "Which country won the FIFA World Cup in 2018?",
+      "options": {
+        "a": "France",
+        "b": "Germany",
+        "c": "Brazil",
+        "d": "Spain"
+      },
+      "correctOption": "a"
+    },
+    {
+      "question": "What is the largest organ in the human body?",
+      "options": {
+        "a": "Liver",
+        "b": "Brain",
+        "c": "Heart",
+        "d": "Skin"
+      },
+      "correctOption": "d"
+    },
+    {
+      "question": "Which artist painted the famous artwork 'The Starry Night'?",
+      "options": {
+        "a": "Pablo Picasso",
+        "b": "Vincent van Gogh",
+        "c": "Salvador Dalí",
+        "d": "Michelangelo"
+      },
+      "correctOption": "b"
+    },
+  ]
+}
